f311c5666085161c6a5e47f1d325269b52201f76
Thirafi Wian.A <thirafi.badai@gmail.com>
2017-11-12 11:22:15 +0700

    TP Modul 8, pls read carefully

---
+++ tpmodul8l.sql
@@ -0,0 +1,26 @@
+drop table pegawai_1301154374;
+select * from BARANG_1301154374;
+select * from CUSTOMER_1301154374;
+select * from PEGAWAI_1301154374;
+select * from PEMBELIAN_1301154374;
+select * from PEMBELIAN_DETIL_1301154374;
+
+/*nomor 1*/
+select id_barang, id_cust, nama, jml, tgl_transaksi from BARANG_1301154374 join PEMBELIAN_DETIL_1301154374 using (id_barang) 
+join PEMBELIAN_1301154374  using (id_transaksi) join CUSTOMER_1301154374 using (id_cust) where id_barang = 'BRG004';
+/*fungsi max*/
+select max(jml) from PEMBELIAN_DETIL_1301154374 where ID_BARANG = 'BRG004';
+
+/*nomor 2*/
+select count(ID_TRANSAKSI),ID_CUST from PEMBELIAN_1301154374 group by id_cust ;
+
+select id_cust, nama, total_bayar from CUSTOMER_1301154374 join PEMBELIAN_1301154374 using(id_cust) having total_bayar < sum(total_bayar);
+
+select id_cust, total_bayar from CUSTOMER_1301154374 join PEMBELIAN_1301154374 using(id_cust) group by id_cust having sum(total_bayar) > total_bayar;
+
+/*nomor 3*/
+select distinct id_cust, nama, total_bayar from CUSTOMER_1301154374 join PEMBELIAN_1301154374 using(id_cust) 
+where total_bayar < (select MAX(TOTAL_BAYAR) from PEMBELIAN_1301154374);
+
+/*check kalo max dari total*/
+select MAX(TOTAL_BAYAR) from PEMBELIAN_1301154374;
